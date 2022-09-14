a98c7b43fdb3f2b814850c1b066b27a4679e26f1
Dave <psychostasia1@gmail.com>
2022-09-14 05:31:10 -0500

    added feeded item rating function

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,7 +1,14 @@
 function FeedbackList({feedback}) {
+    if (!feedback || feedback.length === 0) {
+        return <p>No Feedback yet</p>
+    }
     console.log(feedback)
   return (
-    <div>List</div>
+    <div className="feedback-list">
+        {feedback.map((item) => (
+            <div className="">{item.rating}</div>
+        ))}
+    </div>
   )
 }
 
