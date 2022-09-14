c7330de480b7d7c4546ef0232a07fa9017b23b87
Dave <psychostasia1@gmail.com>
2022-09-14 04:59:59 -0500

    added feedback list items

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,8 @@
+function FeedbackList({feedback}) {
+    console.log(feedback)
+  return (
+    <div>List</div>
+  )
+}
+
+export default FeedbackList
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,24 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam velit iusto nisi cupiditate hic quam fugiat cumque debitis consectetur odit est optio, eveniet ullam quis, saepe autem. Consectetur, quas voluptas.'
+    },
+    {
+        id: 2,
+        rating: 7,
+        text: 'elit. Nam velit iusto nisi cupiditate hic quam fugiat cumque debitis consectetur odit est optio, eveniet ullam quis, saepe autem. Consectetur, quas voluptas.'
+    },
+    {
+        id: 3,
+        rating: 9,
+        text: 'Nam velit iusto nisi cupiditate hic quam fugiat cumque debitis consectetur odit est optio, eveniet ullam quis, saepe autem. Consectetur, quas voluptas.'
+    },
+    {
+        id: 4,
+        rating: 10,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam velit iusto nisi cupiditate hic quam fugiat cumque debitis consectetur odit est optio, eveniet ullam quis, saepe autem. Consectetur, quas voluptas.'
+    }
+]
+
+export default FeedbackData
