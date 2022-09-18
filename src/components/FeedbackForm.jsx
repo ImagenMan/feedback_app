aa70dfdc27ce183caa2f174aaaef73a78e6fe536
Dave <psychostasia1@gmail.com>
2022-09-18 16:23:43 -0500

    button validation active

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,7 +1,50 @@
+// import { cloneElement } from "react"
+import { useState } from "react"
 import Card from "./shared/Card"
+import Button from "./shared/Button"
+
 function FeedbackForm() {
+    const [text, setText] = useState('')
+    const [btnDisabled, setbtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
+
+    const handleTextChange = (e) => {
+        if(text === '') {
+            setbtnDisabled(true)
+            setMessage(null)
+        } else if(text !== '' && text.trim().length <= 10) {
+            setMessage('must contain 10 characters or more')
+            setbtnDisabled(true)
+        } else {
+            setMessage(null)
+            setbtnDisabled(false)
+        }
+
+        setText(e.target.value)
+    }
+
   return (
-    <Card>Form</Card>
+    <Card>
+        <form>
+            <h2>How would you rate this?</h2>
+            {/* @todo rating select comonent */}
+            <div className="input-group">
+                <input 
+                onChange={handleTextChange} 
+                type="text" 
+                placeholder="Write a review" 
+                value={text}
+                />
+                <Button 
+                    type="submit" 
+                    isDisabled={btnDisabled}
+                    >Send
+                </Button>
+            </div>
+
+            {message && <div className="message">{message}</div>}
+        </form>
+    </Card>
   )
 }
 
